222896bd4013de1706808ed849f47df807482cdb
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2022-04-26 18:34:42 +0530

    changed background

---
+++ src/UserPage/Components/Event_Card/data.js
@@ -0,0 +1,11 @@
+const events =[
+    {
+        society : 'INSTINCT',
+        eventname: 'Resume Review Session',
+        date: '12 April,22',
+        time: '6:30 pm',
+        id:1,
+    }
+]
+
+export default events;
--- src/UserPage/Components/Header/Header.js
+++ src/UserPage/Components/Header/Header.js
@@ -3,39 +3,77 @@ import "./header.css"
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa"
 import Heading from './Heading';
+import '../Event_Card/Card';
+import Card from '../Event_Card/Card';
+
 export default function Header() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">About</a>
-                            </li>
-                            
-                        </ul>
-                        <form className="d-flex" id="navside">
-                            <BsFillPersonPlusFill />
-                            <button>Login</button>
-                            <FaPlus />
-                            <button>Register</button>
-                            <i className="fa fa-sign-in" aria-hidden="true"></i>
-                        </form>
+            <div className='stars'>
+                <div className="twinkling">
+                    <div className="clouds">
+                        <div className="navbar">
+                            <div className="container-fluid">
+                                <a href="/" className="navbar-brand"><h2>Home</h2></a>
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item">
+                                            <a href="/" className="nav-link active" aria-current="page">About</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="/" className="nav-link active" aria-current="page">Socities</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="/" className="nav-link active" aria-current="page">FAQS</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </nav>
-            <div>
-                <Heading/>
             </div>
-
         </div>
+
+
+        // <div>
+        //     <nav className="navbar navbar-expand-lg navbar-light">
+        //         <div className="container-fluid">
+        //             <a className="navbar-brand" href="/">Navbar</a>
+        //             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //                 <span className="navbar-toggler-icon"></span>
+        //             </button>
+        //             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        //                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        //                     <li className="nav-item">
+        //                         <a className="nav-link active" aria-current="page" href="/">Home</a>
+        //                     </li>
+        //                     <li className="nav-item">
+        //                         <a className="nav-link active" aria-current="page" href="/">About</a>
+        //                     </li>
+                            
+        //                 </ul>
+        //                 <form className="d-flex" id="navside">
+        //                     <BsFillPersonPlusFill />
+        //                     <button>Login</button>
+        //                     <FaPlus />
+        //                     <button>Register</button>
+        //                     <i className="fa fa-sign-in" aria-hidden="true"></i>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </nav>
+        //     <div>
+        //         <Heading/>
+        //     </div>
+        //     {/* <div className="events">
+        //         <Card />
+        //     </div> */}
+
+
+        // </div>
     )
 }
