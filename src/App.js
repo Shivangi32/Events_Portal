f7c67bebe0d48737ad13800aec756ecd68548f4e
Shivangi32 <sdatisha.2195@gmail.com>
2022-05-30 23:08:31 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 // import Header from "./UserPage/Components/Header/Header";
 import SocietyPage from "./Society/SocietyPage";
 import UserPage from "./UserPage/Components/Header/Header";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { BrowserRouter as  Router } from "react-router-dom";
 // import Heading from "./UserPage/Components/Header/Heading";
 
 function App() {
   return (
-    <>
+    <Router>
       {/* <Header /> */}
 
       <div className="societyPage-background">
         <UserPage />
         <SocietyPage />
       </div>
-    </>
+    </Router>
   );
 }
 
