8b563d450af7b30f562a6455bbb874840fcc9e14
Shivangi32 <sdatisha.2195@gmail.com>
2022-05-30 22:40:25 +0530

    user page linked

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 // import Header from "./UserPage/Components/Header/Header";
 import SocietyPage from "./Society/SocietyPage";
+import UserPage from "./UserPage/Components/Header/Header";
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Heading from "./UserPage/Components/Header/Heading";
 
@@ -10,6 +11,7 @@ function App() {
       {/* <Header /> */}
 
       <div className="societyPage-background">
+        <UserPage />
         <SocietyPage />
       </div>
     </>
