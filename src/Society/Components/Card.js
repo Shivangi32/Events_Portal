918bbd0e3398c5eaa9484956abf362463855f267
Nishtha Agarwal <only4nishtha@gmail.com>
2022-06-25 12:38:11 +0530

    Minor Run changes

--- src/Society/Components/Card.js
+++ src/Society/Components/Card.js
@@ -1,3 +1,4 @@
+import Modal from "react-modal";
 
 function Card(props) {
   return (
@@ -8,7 +9,9 @@ function Card(props) {
             <img src="" alt="logo" />
           </div>
           <div className="icons">
-            <div className="material-icons">close</div>
+            <div className="material-icons" 
+            // onClick={() => editModal( id: )}
+            >close</div>
             <div className="material-icons">edit</div>
           </div>
         </div>
@@ -25,3 +28,64 @@ function Card(props) {
 }
 
 export default Card;
+
+// class Card extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       editing: false
+//     };
+//     this.newDate = "";
+//     this.newTime = "";
+//     this.newEventName = "";
+//   }
+
+//   render() {
+//     const { imgSrc, cardName, birthYear, homeWorld, onEdit } = this.props;
+//     return (
+//       <div className="card">
+//         <div className="card-content">
+//           <div className="card-name">{cardName}</div>
+//           <img src={`http://localhost:3008/${imgSrc}`} alt="profile" />
+//           <p>
+//             <span>Birthday:</span>
+//             {this.state.editing ? (
+//               <span className="birth-year">{birthYear}</span>
+//             ) : (
+//               <input
+//                 type="text"
+//                 defaultValue={birthYear}
+//                 ref={node => {
+//                   this.newbirthYear = node;
+//                 }}
+//               />
+//             )}
+//           </p>
+//           <p>
+//             <span>Homeworld:</span>
+//             {this.state.editing ? (
+//               <span className="home-world">{homeWorld}</span>
+//             ) : (
+//               <input
+//                 type="text"
+//                 defaultValue={homeWorld}
+//                 ref={node => {
+//                   this.newHomeWorld = node;
+//                 }}
+//               />
+//             )}
+//           </p>
+//           <div align="center">
+//             <button
+//               onClick={() => {
+//                 this.setState({ editing: true });
+//               }}
+//             >
+//               Edit
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
