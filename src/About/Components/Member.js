d6860c679a356d6ebb03aac17a5f7f61df59b375
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2022-07-16 16:53:03 +0530

    about page changes

--- src/About/Components/Member.js
+++ src/About/Components/Member.js
@@ -1,31 +1,26 @@
-import React from 'react'
-import "./Member.css" 
-import Team from "./Team"
+import React from "react";
+import "./Member.css";
+import Team from "./Team";
 export default function Member() {
   return (
-<div className='team'>
-<div  class="container">
-{Team.map((item,index)=>{
-    return(
-        
-        <div key={index} class="card">
-            <div class="content">
+    <div className="team">
+      <div class="container">
+        {Team.map((item, index) => {
+          return (
+            <div key={index} class="card">
+              <div class="content">
                 <div class="imgbox">
-                    <img src={item.img} alt=""/>
+                  <img src={item.img} alt="" />
                 </div>
                 <div class="detail">
-                    <h4>{item.name}</h4>
-                    <h5>{item.position}</h5>
+                  <h4>{item.name}</h4>
+                  <h5>{item.position}</h5>
                 </div>
+              </div>
             </div>
-        </div>
-    
-    )
-
-})}
-</div>
-</div>
-
-    
-  )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
