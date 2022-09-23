1487408b81007b0a96dd28f67f6a6c548bfd403a
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2022-09-23 18:00:26 +0530

    date modified

--- src/UserPage/Components/Event_Card/Card.js
+++ src/UserPage/Components/Event_Card/Card.js
@@ -1,41 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import './Card.css';
-import img from './img.PNG'
-import {
-    query,
-    getDocs,
-    collection,
-    where, doc
-} from "firebase/firestore";
-import { db } from "../../../firebaseConfig"
-
+import React, { useState, useEffect } from "react";
+import "./Card.css";
+import img from "./img.PNG";
+import { query, getDocs, collection, where, doc } from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
 
 export const Event = ({ event: { soc, EventName, date, time } }) => {
-    return (
-        <div className="event">
-            <div className="event_wrapper">
-                <img className="event-img" src={img}/>
-                <div className='info'>
-                <div className="event_name">
-                    <span>{EventName.toUpperCase()}</span>
-                </div>
-                <div className="event_society">
-                    <span className="society_name">{soc}</span>
-                </div>
-                
-                <div className="event_date">
-                    <span className="date">{date} , {time}</span>
-                </div>
-                <div className="event_register">
-                    <button><span className='register' >REGISTER</span></button>
-                </div>
-                </div>
-            </div>
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return (
+    <div className="event">
+      <div className="event_wrapper">
+        <img className="event-img" src={img} />
+        <div className="info">
+          <div className="event_name">
+            <span>{EventName.toUpperCase()}</span>
+          </div>
+          <div className="event_society">
+            <span className="society_name">{soc}</span>
+          </div>
+
+          <div className="event_date">
+            <span className="date">
+              {date.slice(0, 2)} {months[parseInt(date.slice(3, 5))]}{" "}
+              {date.slice(6, 10)}, {time}
+            </span>
+          </div>
+          <div className="event_register">
+            <button>
+              <span className="register">REGISTER</span>
+            </button>
+          </div>
         </div>
-    )
-}
-
-
+      </div>
+    </div>
+  );
+};
 
 /*const Card = () => {
 
