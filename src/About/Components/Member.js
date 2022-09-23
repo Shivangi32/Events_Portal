cb4c0d56888d6b84775f10012304569ec02a2a53
Nishtha Agarwal <77295838+only4nishtha@users.noreply.github.com>
2022-09-23 18:48:40 +0530

    About page

--- src/About/Components/Member.js
+++ src/About/Components/Member.js
@@ -1,33 +1,32 @@
 import React from "react";
 import "./Member.css";
 import Team from "./Team";
-import twitter from "../images/twitter.png";
-import instagram from "../images/instagram.png";
-import linkedin from "../images/linkedin.png";
+// import twitter from "../images/twitter.png";
+// import instagram from "../images/instagram.png";
+// import linkedin from "../images/linkedin.png";
 
 export default function Member() {
   return (
-    <div className="team">
-      <div className="Header">
+    <div className="about_team">
+      <div className="about_Header">
         <h1>TEAM</h1>
       </div>
-      <div className="container">
+      <div className="about_container">
         {Team.map((item, index) => {
           return (
-            <div key={index} className="card">
-              <div className="card-inner">
-                <div className="content">
-                  <div className="imgbox">
+            <div key={index} className="about_card">
+              <div className="about_card-inner">
+                <div className="about_content">
+                  <div className="about_imgbox">
                     <img src={item.img} alt="" />
                   </div>
                 </div>
-                <div className="flip">
-                  <div className="detail">
+                <div className="about_flip">
+                  <div className="about_detail">
                     <h4>{item.name}</h4>
                     <h5>{item.position}</h5>
                     <p>{item.description}</p>
-                    <div className="social">
-                      {/* <button className='twitter'></button> */}
+                    {/* <div className="about_social">
                       <a href="{item.twitter}">
                         <img src={twitter}></img>
                       </a>
@@ -37,9 +36,7 @@ export default function Member() {
                       <a href="{item.linkedin}">
                         <img src={linkedin}></img>
                       </a>
-                      {/* <a href='{item.instagram}'><i class="fa-brands fa-instagram purple"></i></a> */}
-                      {/* <a href='{item.linkedin}'><i class="fa-brands fa-linkedin purple"></i></a> */}
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
