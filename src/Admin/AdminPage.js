e069221c84779230f66346c5f3b32b260eb5c03c
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2022-10-20 19:18:45 +0530

    admin navbar

--- src/Admin/AdminPage.js
+++ src/Admin/AdminPage.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import Name from "./Components/Name/Name"
-import Title from "./Components/Title/Title"
-import ReviewPosts from "./Components/ReviewPosts/Posts"
-import StatsCard from "./Components/Stats/StatsCard"
-import Searchbar from './Components/Searchbar/Searchbar';
+import React from "react";
+import Name from "./Components/Name/Name";
+import Title from "./Components/Title/Title";
+import ReviewPosts from "./Components/ReviewPosts/Posts";
+import StatsCard from "./Components/Stats/StatsCard";
+import Searchbar from "./Components/Searchbar/Searchbar";
 // import './src/App.css'
 // import Home from './pages/Home';
 // import Events from './pages/Events'
 
-import Statistics from './Components/StatsCounter/statsCounter';
+import Statistics from "./Components/StatsCounter/statsCounter";
 
-const admin='admin';
-const App = ({setShowNavFunc}) => {
-  setShowNavFunc(false);
+const admin = "admin";
+const AdminPage = ({ setShowNavFunc }) => {
+  setShowNavFunc(true);
   return (
-  <>
-
-    <Searchbar />
-    <Name admin={admin}/>
-    <Statistics />
-    <Title/>
-    {/* <StatsCard/> */}
-    <ReviewPosts />
-  </>   
+    <>
+      <Searchbar />
+      <Name admin={admin} />
+      <Statistics />
+      <Title />
+      {/* <StatsCard/> */}
+      <ReviewPosts />
+    </>
   );
-}
+};
 
-export default App;
+export default AdminPage;
