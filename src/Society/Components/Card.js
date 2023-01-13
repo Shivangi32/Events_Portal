c0b7b34508a8e07fa957b2368d6381fc2ae9c39d
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2023-01-13 21:04:48 +0530

    fixed cards

--- src/Society/Components/Card.js
+++ src/Society/Components/Card.js
@@ -7,7 +7,6 @@ import { useState, useEffect } from "react";
 import { MdDelete, MdModeEdit } from "react-icons/md";
 import { getAccordionActionsUtilityClass } from "@mui/material";
 
-
 function Card(props) {
   let info = {
     id: props.id,
@@ -18,7 +17,7 @@ function Card(props) {
     time: props.time,
     link: props.link,
     approved: props.approved,
-    category: props.category
+    category: props.category,
   };
   const [openEditModal, setopenEditModal] = useState(false);
 
@@ -43,28 +42,28 @@ function Card(props) {
   ];
 
   function getMeridian(time) {
-    if(time >= "12:00") {
-      return "PM"
+    if (time >= "12:00") {
+      return "PM";
     } else {
-      return "AM"
+      return "AM";
     }
   }
 
   function getTime(time) {
-    let hh = parseInt(time.slice(0,2))
-    if(hh > 12) {
-      hh = hh % 12
-      if(hh < 10) {
-        return String("0" + hh + ":" + time.slice(3, 6))
+    let hh = parseInt(time.slice(0, 2));
+    if (hh > 12) {
+      hh = hh % 12;
+      if (hh < 10) {
+        return String("0" + hh + ":" + time.slice(3, 6));
       }
-      return String(hh + ":" + time.slice(3, 6))
+      return String(hh + ":" + time.slice(3, 6));
     }
-    return time
+    return time;
   }
   return (
-    <div className="card">
-      <div className="event_wrapper">
-        <img className="event-img" src={img} />
+    <div className="card col-3 col-md-5">
+      <div className="card_wrapper">
+        <img className="card-img" src={img} />
         <div className="info">
           <div className="event_name">
             <span>{props.EventName.toUpperCase()}</span>
@@ -75,20 +74,24 @@ function Card(props) {
 
           <div className="event_date">
             <span className="date">
-              {props.date.slice(8, 10)} {months[parseInt(props.date.slice(5, 7))]}{" "}
-              {props.date.slice(0, 4)}{", "} 
+              {props.date.slice(8, 10)}{" "}
+              {months[parseInt(props.date.slice(5, 7))]}{" "}
+              {props.date.slice(0, 4)}
+              {", "}
             </span>
-            <span className="time">{getTime(props.time)} {getMeridian(props.time)}{" IST"}</span>
+            <span className="time">
+              {getTime(props.time)} {getMeridian(props.time)}
+              {" IST"}
+            </span>
           </div>
         </div>
         <div className="event-action">
           <div className="event_register">
             <a href={props.link} target="_blank">
-            <button>
-              <span className="register">REGISTER</span>
-            </button>
+              <button>
+                <span className="register">REGISTER</span>
+              </button>
             </a>
-            
           </div>
           <div className="icon event_edit">
             <MdModeEdit
--- src/UserPage/Components/UserPage.js
+++ src/UserPage/Components/UserPage.js
@@ -2,19 +2,17 @@ import Heading from "./Heading";
 import Card from "./Event_Card/Card.js";
 import SearchBar from "./Search_Bar/searchBar";
 import Sidebar from "../../Sidebar/Sidebar";
-import "./UserPage.css"
+import "./UserPage.css";
 
 export default function Header({ setShowNavFunc }) {
   setShowNavFunc(true);
 
   return (
-    <div >
-    <Heading />
-    <SearchBar />
-      
-      
-      
-      {/*<Card />*/}
+    <div>
+      <Heading />
+      <SearchBar />
+
+      {/* <Card /> */}
     </div>
   );
 }
